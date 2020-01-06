a56c7a3e8b2a617c50bd5295a3fbab73a84a624d
John Ordoyo <gtom_1984@hotmail.ca>
2020-01-05 21:46:07 -0800

    42_Basic_Input_Binding
    -Added input to rocket with rocket.cs

---
+++ Assets/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();   
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Thrusters initiated"); 
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Rotate Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Rotate Right");
+        }
+    }
+}
