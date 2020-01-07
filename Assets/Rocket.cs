19762486e7773a3fa8f5c862428f3ec82d73db4c
John Ordoyo <gtom_1984@hotmail.ca>
2020-01-07 00:28:03 -0800

    45_Using_Time.deltaTime
    - We can rotate the rocket now!

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,10 +7,15 @@ public class Rocket : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody rigidBody;
+    Transform transform;
     int multiplier = 5;
+    Vector3 rotation;
+    float angle = 2;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        transform = GetComponent<Transform>();
+        
     }
 
     // Update is called once per frame
@@ -21,19 +26,21 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
+        rotation = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up * multiplier);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rigidBody.AddRelativeForce(Vector3.right * -multiplier);
+            transform.Rotate(Vector3.forward);
             Debug.Log("Rotate Left");
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            rigidBody.AddRelativeForce(Vector3.right * multiplier);
+            transform.Rotate(-Vector3.forward);
             Debug.Log("Rotate Right");
         }
+        
     }
 }
