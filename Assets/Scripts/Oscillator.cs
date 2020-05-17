d1cc0d01aa517c4e77b862cbb9c3b414ec0c8d1f
jordoyo <john_ordoyo@sfu.ca>
2020-05-16 22:43:07 -0700

    Finish Video 61
    Add oscillator movement in some moving obstacles for both levels

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Vector3 movementVector;
+
+    //TODO remove from inspector
+    [Range(0, 1)][SerializeField] float movementFactor;
+
+    Vector3 startingPosition;
+
+
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPosition + offset;
+    }
+}
