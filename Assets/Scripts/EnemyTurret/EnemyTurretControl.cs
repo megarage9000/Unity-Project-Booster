f1146ee2c3263d92c46bbd6b36850fde34e1c203
jordoyo <john_ordoyo@sfu.ca>
2020-05-25 21:00:16 -0700

    Started EnemyTurretControl Script
    - Need to work on tracking the enemy and scanning!

--- Assets/Scripts/EnemyTurret/EnemyTurretControl.cs
+++ Assets/Scripts/EnemyTurret/EnemyTurretControl.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class EnemyTurretControl : TurretControl
 {
+
+    [SerializeField] float turretScanRange = 50f;
+    BoxCollider turretScanArea;
+
+    private void Awake()
+    {
+        turretScanArea = GetComponent<BoxCollider>();
+
+        Vector3 initialAreaCenter = turretScanArea.center;
+        Vector3 initialAreaSize = turretScanArea.size;
+
+        turretScanArea.center.Set(initialAreaCenter.x, initialAreaCenter.y + turretScanRange / 2, initialAreaCenter.z);
+        turretScanArea.size.Set(initialAreaSize.x, turretScanRange, initialAreaSize.z);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Detected an object!");
+    }
     protected override void OperateTurret()
     {
-        throw new System.NotImplementedException();
+           
     }
 
     protected override void RotateTurret()
     {
-        throw new System.NotImplementedException();
+        
     }
 
+    private void scan()
+    {
+
+    }
+
+    
 
 }
